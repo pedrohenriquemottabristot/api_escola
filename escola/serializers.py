3b7c1117d633ddd132368a2e2bd19e699c791ffa
PEDRO HENRIQUE MOTTA BRISTOT <pedro_bristot@estudante.sesisenai.org.br>
2024-08-21 21:08:24 -0300

    Initial commit

---
+++ escola/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from escola.models import Estudante, Curso, Matricula
+
+
+class EstudanteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Estudante
+        fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
+
+
+# código omitido
+
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+
+        fields = '__all__'
+
+from escola.models import Estudante,Curso, Matricula
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
+
+
+class ListaMatriculasEstudanteSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+
+    class Meta:
+
+        model = Matricula
+        fields = ['curso', 'periodo']
+
+    def get_periodo(self, obj):
+
+        return obj.get_periodo_display()
+
+
+class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
+    estudante_nome = serializers.ReadOnlyField(source='estudante.nome')
+    class Meta:
+        model = Matricula
+        fields = ['estudante_nome']
